44f7a449ef9df6a8cec0ae2c9b611ca0360ccbe2
Carlos Filoteo <cfiloteo@ancestry.com>
2017-11-07 22:38:07 -0700

    Add fontSizeInput controls

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
     document.addEventListener('DOMContentLoaded', () => {
         const style = getComputedStyle(document.documentElement) // Initial custom property values are obtained via computedStyles
         const themeOption = document.querySelectorAll('[name="themeOption"]')
+        const fontSizeInput = document.getElementById('fontSizeInput')
 
         selectTheme = ({target: {value}}) => {
             const themePrimary = style.getPropertyValue(`--${value}-primary`).trim()
             const themeSecondary = style.getPropertyValue(`--${value}-secondary`).trim()
-            
+
             document.documentElement.style.setProperty('--primary', themePrimary)
-            document.documentElement.style.setProperty('--secondary', themeSecondary) 
+            document.documentElement.style.setProperty('--secondary', themeSecondary)
         }
 
         themeOption.forEach(option => {
             option.addEventListener('click', selectTheme)
+        })
+
+        fontSizeInput.addEventListener('input', ({target: {value}}) => {
+            document.documentElement.style.fontSize = `${value}px`
         })
     });
 })()
